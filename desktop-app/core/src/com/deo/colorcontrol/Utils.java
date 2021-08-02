192137806b038425a75215c0caabf9a28a1d241c
kloud <dgudim@gmail.com>
2021-08-02 20:35:18 +0300

    Minor changes

--- desktop-app/core/src/com/deo/colorcontrol/Utils.java
+++ desktop-app/core/src/com/deo/colorcontrol/Utils.java
@@ -6,7 +6,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Array;
 
+import java.util.Arrays;
+
+import static com.badlogic.gdx.math.MathUtils.clamp;
 import static java.lang.StrictMath.abs;
 
 public class Utils {
@@ -70,4 +74,61 @@ public class Utils {
             }
         }
     }
+    
+    static void fillArray(float[] array, float value){
+        Arrays.fill(array, value);
+    }
+    
+    static float addValueToAShiftingArray(int maxSize, float valueToAdd, Array<Float> targetArray, boolean clamp) {
+        float average = 0;
+        if (targetArray.size < maxSize) {
+            targetArray.add(valueToAdd);
+            for (int i = 0; i < targetArray.size; i++) {
+                average += targetArray.get(i);
+            }
+        } else {
+            for (int i = 0; i < targetArray.size - 1; i++) {
+                targetArray.set(i, targetArray.get(i + 1));
+                average += targetArray.get(i + 1);
+            }
+            targetArray.set(targetArray.size - 1, valueToAdd);
+            average += valueToAdd;
+        }
+        average /= (float) targetArray.size;
+        if (clamp) {
+            average = clamp(average, 0, 1);
+        }
+        return average;
+    }
+    
+    static float[] addValueToAShiftingArray(int maxSize, float[] valueToAdd, Array<float[]> targetArray, boolean clamp) {
+        float[] average = new float[valueToAdd.length];
+        if (targetArray.size < maxSize) {
+            targetArray.add(valueToAdd);
+            for (int i = 0; i < targetArray.size; i++) {
+                for (int a = 0; a < average.length; a++) {
+                    average[a] += targetArray.get(i)[a];
+                }
+            }
+        } else {
+            for (int i = 0; i < targetArray.size - 1; i++) {
+                targetArray.set(i, targetArray.get(i + 1));
+                for (int a = 0; a < average.length; a++) {
+                    average[a] += targetArray.get(i + 1)[a];
+                }
+            }
+            targetArray.set(targetArray.size - 1, valueToAdd);
+            for (int a = 0; a < average.length; a++) {
+                average[a] += valueToAdd[a];
+            }
+        }
+        for (int a = 0; a < average.length; a++) {
+            average[a] /= (float) targetArray.size;
+            if (clamp) {
+                average[a] = clamp(average[a], 0, 1);
+            }
+        }
+        return average;
+    }
+    
 }
