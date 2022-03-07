59a2e0dc91edfd7ea947cfa20db125aa17838100
kloud <dgudim@gmail.com>
2022-03-07 13:10:51 +0300

    ui tweaks

--- desktop-app/desktop/src/com/deo/colorcontrol/desktop/DesktopLauncher.java
+++ desktop-app/desktop/src/com/deo/colorcontrol/desktop/DesktopLauncher.java
@@ -1,24 +1,17 @@
 package com.deo.colorcontrol.desktop;
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.deo.colorcontrol.Main;
 
 public class DesktopLauncher {
 	
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.pauseWhenBackground = false;
-		config.width = 800;
-		config.height = 480;
-		config.resizable = false;
-		config.foregroundFPS = 60;
-		config.backgroundFPS = 60;
-		config.vSyncEnabled = true;
-		config.forceExit = true;
-		config.addIcon("spectrum.png", Files.FileType.Internal);
-		config.title = "Arduino color music controller";
-		new LwjglApplication(new Main(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(800, 480);
+		config.setResizable(false);
+		config.setWindowIcon("spectrum.png");
+		config.setTitle("Arduino color music controller");
+		new Lwjgl3Application(new Main(), config);
 	}
 }
