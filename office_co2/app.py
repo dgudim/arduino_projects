a6ce53e4586a1c6ba1eb2fcba08b349c6c4adeba
dgudim <dgudim@gmail.com>
2024-08-31 18:03:39 +0300

    Backups and floating average

--- office_co2/app.py
+++ office_co2/app.py
@@ -11,16 +11,20 @@ import streamlit as st
 
 ONE_DAY_S = 86400
 
+
 def to_sec_unix(dt: datetime | date):
     if isinstance(dt, datetime):
         return dt.replace(tzinfo=UTC).timestamp()
-    return calendar.timegm(dt.timetuple()) + ONE_DAY_S # Extend till the end of the day
+    return calendar.timegm(dt.timetuple()) + ONE_DAY_S  # Extend till the end of the day
+
 
 def from_sec_unix(s: float):
     return datetime.fromtimestamp(s, UTC)
 
+
 def now():
     return datetime.now(UTC)
+
 
 db = apsw.Connection("./hist/history.db", flags=apsw.SQLITE_OPEN_READONLY)
 db.set_busy_timeout(300)
@@ -29,7 +33,9 @@ apsw.ext.log_sqlite()
 timestamp_now = to_sec_unix(now())
 one_week_ago = timestamp_now - ONE_DAY_S * 7
 
-date_range = st.date_input("Data range", [from_sec_unix(one_week_ago), from_sec_unix(timestamp_now)])
+date_range = st.date_input(
+    "Data range", [from_sec_unix(one_week_ago), from_sec_unix(timestamp_now)]
+)
 datetuple_raw = cast(tuple[date, date] | tuple[date], date_range)
 
 if len(datetuple_raw) == 1:
@@ -37,81 +43,74 @@ if len(datetuple_raw) == 1:
 else:
     datetuple = cast(tuple[date, date], datetuple_raw)
 
-spacing = ((to_sec_unix(datetuple[1]) - to_sec_unix(datetuple[0])) / ONE_DAY_S) / 30 # every month, the scaling increases by 1
+spacing = int(
+    ((to_sec_unix(datetuple[1]) - to_sec_unix(datetuple[0])) / ONE_DAY_S) / 30 + 1
+)  # every month, the scaling increases by 1
+
 
 cursor = db.cursor()
-cursor.execute(f"select value, dt from co2 where dt >= {to_sec_unix(datetuple[0])} AND dt <= {to_sec_unix(datetuple[1])}")
+cursor.execute(
+    f"select value, dt from co2 where dt >= {to_sec_unix(datetuple[0])} AND dt <= {to_sec_unix(datetuple[1])} AND rowid % {spacing} = 0"
+)
 co2_df = pd.DataFrame(cast(list[tuple[int, int]], cursor.fetchall()))
 
-cursor.execute(f"select value, dt from temp where dt > {to_sec_unix(datetuple[0])} AND dt <= {to_sec_unix(datetuple[1])}")
+cursor.execute(
+    f"select value, dt from temp where dt > {to_sec_unix(datetuple[0])} AND dt <= {to_sec_unix(datetuple[1])} AND rowid % {spacing} = 0"
+)
 temp_df = pd.DataFrame(cast(list[tuple[int, int]], cursor.fetchall()))
+
+def make_chart(df: pd.DataFrame, data_range: list[int], main_col: str, line_col: str, rolling_mean_col: str):
+    transformed_data = pd.DataFrame(
+        {
+            "time": df[df.columns[1]].map(
+                datetime.fromtimestamp, na_action="ignore"
+            ),
+            "data": df[df.columns[0]],
+        }
+    )
+
+    main_chart = (
+        alt.Chart()
+        .mark_line(color=main_col)
+        .encode(
+            x=alt.X("time:T", axis=alt.Axis(format="%a %d %H:%M")),
+            y=alt.Y("data", scale=alt.Scale(domain=data_range)),
+        )
+        .interactive()
+    )
+
+    mean_line = (
+        alt.Chart()
+        .mark_rule(color=line_col)
+        .encode(y="mean(data):Q", size=alt.SizeValue(3))
+    )
+
+    rolling_mean= (
+        alt.Chart()
+        .mark_line(color=rolling_mean_col)
+        .transform_window(
+            # The field to average
+            rolling_mean="mean(data)",
+            # The number of values before and after the current value to include.
+            frame=[-9, 0],
+        )
+        .encode(x="time:T", y="rolling_mean:Q")
+    )
+
+    st.altair_chart(
+        cast(alt.Chart, alt.layer(main_chart, mean_line, rolling_mean, data=transformed_data)),
+        use_container_width=True,
+    )
 
 
 st.subheader("Temperature history")
-
 if len(temp_df) == 0:
     st.write("No temperature history for selected period")
 else:
-    temp_data = pd.DataFrame(
-        {
-            "time": temp_df[temp_df.columns[1]].map(
-                datetime.fromtimestamp, na_action="ignore"
-            ),
-            "temperature": temp_df[temp_df.columns[0]],
-        }
-    )
-
-    temp_chart = (
-        alt.Chart()
-        .mark_line(color="#FF5500")
-        .encode(
-            x=alt.X("time:T", axis=alt.Axis(format="%a %d %H:%M")),
-            y=alt.Y("temperature", scale=alt.Scale(domain=[22, 30])),
-        )
-        .interactive()
-    )
-
-    temp_line = (
-        alt.Chart()
-        .mark_rule(color="firebrick")
-        .encode(y="mean(temperature):Q", size=alt.SizeValue(3))
-    )
-
-    st.altair_chart(
-        alt.layer(temp_chart, temp_line, data=temp_data), use_container_width=True
-    )
+    make_chart(temp_df, [20, 30], "#FF5500", "#b22222", "#ffc261")
 
 st.subheader("Co2 history")
-
 if len(co2_df) == 0:
     st.write("No co2 history for selected period")
 else:
-
-    co2_data = pd.DataFrame(
-        {
-            "time": co2_df[co2_df.columns[1]].map(
-                datetime.fromtimestamp, na_action="ignore"
-            ),
-            "co2": co2_df[co2_df.columns[0]],
-        }
-    )
-
-    co2_chart = (
-        alt.Chart()
-        .mark_line(color="#2255FF")
-        .encode(
-            x=alt.X("time:T", axis=alt.Axis(format="%a %d %H:%M")),
-            y=alt.Y("co2", scale=alt.Scale(domain=[300, 900])),
-        )
-        .interactive()
-    )
-
-    co2_line = (
-        alt.Chart()
-        .mark_rule(color="teal")
-        .encode(y="mean(co2):Q", size=alt.SizeValue(3))
-    )
-
-    st.altair_chart(
-        alt.layer(co2_chart, co2_line, data=co2_data), use_container_width=True
-    )
+    make_chart(co2_df, [300, 900], "#2255FF", "#008080", "#65f0cd")
--- office_co2/hist/history.sh
+++ office_co2/hist/history.sh
@@ -1,6 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
+BACKUPS_PATH="backups"
+HIST_FILE="history.db"
+MAX_BACKUPS=10
 granularity=90
+backup_interval=960 # (960 * granularity = 86400 (1 day))
+
+function do_backup() {
+  sqlite3 $HIST_FILE .dump | gzip -c >"$BACKUPS_PATH/$HIST_FILE.backup-$(date +%s).gz"
+  # zcat history.db.backup-N.gz | sqlite3 ./history.db
+
+  # Create an array of backup directories sorted by date (newest to oldest)
+  local backups=($(ls -1d $BACKUPS_PATH/* | sort -r))
+
+  # Keep only the newest $MAX_BACKUPS directories
+  local count=${#backups[@]}
+  if [ $count -gt $MAX_BACKUPS ]; then
+    local backups_to_delete=(${backups[@]:$MAX_BACKUPS})
+    for backup in "${backups_to_delete[@]}"; do
+      rm -rf "$backup"
+    done
+  fi
+}
 
 if [ ! -f ./CntR ]; then
   echo "CntR not found!"
@@ -12,9 +33,11 @@ if [ ! -f ./Tamb ]; then
   exit 1
 fi
 
-if [ ! -f ./history.db ]; then
+mkdir -pv $BACKUPS_PATH
+
+if [ ! -f $HIST_FILE ]; then
   echo "Creating database..."
-  sqlite3 ./history.db <<EOF
+  sqlite3 $HIST_FILE <<EOF
 PRAGMA journal_mode = WAL;
 
 create table co2 (rowid INTEGER PRIMARY KEY, value int, dt datetime);
@@ -24,14 +47,20 @@ fi
 
 echo "Starting main loop, granularity: ${granularity}s"
 
+i=0
+
 while true; do
-    co2_level=$(cat ./CntR)
-    temp_level=$(cat ./Tamb)
+  co2_level=$(cat ./CntR)
+  temp_level=$(cat ./Tamb)
 
-    sqlite3 ./history.db "INSERT INTO co2(value, dt) VALUES($co2_level, $(date +%s));"
-    sqlite3 ./history.db "INSERT INTO temp(value, dt) VALUES($temp_level, $(date +%s));"
+  sqlite3 $HIST_FILE "INSERT INTO co2(value, dt) VALUES($co2_level, $(date +%s));"
+  sqlite3 $HIST_FILE "INSERT INTO temp(value, dt) VALUES($temp_level, $(date +%s));"
 
-    sleep $granularity
+  if [ "$i" -eq "$backup_interval" ]; then
+    do_backup
+    i=0
+  fi
+  i=$((i + 1))
+
+  sleep $granularity
 done
-
-
